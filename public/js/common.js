89b7b43d3d16fd08a77fd5f0974884d7860b9973
MaxSimakov <Simakov.maxim82@gmail.com>
2022-11-02 18:18:11 +0300

    edit sProducts

--- public/js/common.js
+++ public/js/common.js
@@ -421,6 +421,20 @@ function eventHandler() {
 		},
 		
 	});
+	const sProductsSwiper = new Swiper('.sProducts__slider--js', {
+		slidesPerView: 3,
+		spaceBetween: 0,
+		// loop: true,
+		freeMode: false,
+		loopFillGroupWithBlank: true,
+		watchOverflow: true,
+		// centeredSlides: false,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		
+	});
 
 	let colTexts = document.querySelectorAll('.sTeam__col-text');
 	console.log();
