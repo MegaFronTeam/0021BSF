26dee017707b2d9fbca676aeec44700caa6b0aa9
Janis <janis.paberzs18@gmail.com>
2023-11-26 17:22:25 +0200

    edit index page

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -448,6 +448,10 @@ function eventHandler() {
 			draggable: true,
 			
 		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
 		breakpoints: {
 			// when window width is >= 320px
 			// 769: {
